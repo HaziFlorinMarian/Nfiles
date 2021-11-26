90111ddc4a5a2fefe3e7d1e55e77ed15e600bf32
HaziFlorinMarian <haziflorinmarian@gmail.com>
2021-11-26 18:43:24 +0200

    Before Changing Permissions Readable Mode

--- Client.c
+++ Client.c
@@ -10,12 +10,12 @@
    
 #define PORT     8080
 #define MAXLINE 1024
-   
-// Driver code
-int main() {
+
+int main()
+{
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from client";
+    char SentMsg[MAXLINE];
+    char RecvMsg[MAXLINE];
     struct sockaddr_in     servaddr;
    
     // Creating socket file descriptor
@@ -25,24 +25,30 @@ int main() {
     }
    
     memset(&servaddr, 0, sizeof(servaddr));
-       
+
     // Filling server information
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(PORT);
     servaddr.sin_addr.s_addr = INADDR_ANY;
        
     int n, len;
-       
-    sendto(sockfd, (const char *)hello, strlen(hello),
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    printf("Hello message sent.\n");
-           
-    n = recvfrom(sockfd, (char *)buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    buffer[n] = '\0';
-    printf("Server : %s\n", buffer);
+
+    while(1)
+    {
+        printf("Enter filename:\t");
+        fgets(SentMsg, MAXLINE, stdin);
+        SentMsg[strlen(SentMsg)-1]='\0';    //Remove newline
+        printf("\n\n");
+        
+        sendto(sockfd, (const char *)SentMsg, strlen(SentMsg), MSG_CONFIRM, (const struct sockaddr *) &servaddr, sizeof(servaddr));
+            
+        n = recvfrom(sockfd, (char *)RecvMsg, MAXLINE, MSG_WAITALL, (struct sockaddr *) &servaddr, &len);
+        RecvMsg[n] = '\0';
+        printf("%s\n", RecvMsg);
+
+        memset(&SentMsg, 0, MAXLINE);   //We're preparing buffer for next request
+        memset(&RecvMsg, 0, MAXLINE);   //We're preparing buffer for next request
+    }
    
     close(sockfd);
     return 0;
--- Server.c
+++ Server.c
@@ -7,15 +7,33 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <sys/stat.h>
    
 #define PORT     8080
 #define MAXLINE 1024
 
+void GetFileMetadata(const char* szPath, char* Out)
+{
+    if (access(szPath, F_OK) != 0 )
+        snprintf(Out, MAXLINE, "File \"%s\" does not exists.\n", szPath);
+    else
+    {
+        struct stat res;
+        stat(szPath, &res);
+
+        int length = 0;
+        length += snprintf(Out+length, MAXLINE, "Filename:\t%s\n", szPath);
+        length += snprintf(Out+length, MAXLINE, "Filesize:\t%d\n", res.st_size);
+        length += snprintf(Out+length, MAXLINE, "Permissions:\t07o\n", res.st_mode); //Octal
+        length += snprintf(Out+length, MAXLINE, "Owner:\t%d\n", res.st_gid);
+    }
+}
+
 int main()
 {
     int sockfd;
-    char buffer[MAXLINE];
-    char *hello = "Hello from server";
+    char InMsg[MAXLINE], OutMsg[MAXLINE];
+
     struct sockaddr_in servaddr, cliaddr;
        
     // Creating socket file descriptor
@@ -46,11 +64,14 @@ int main()
 
     while(1)
     {
-        n = recvfrom(sockfd, (char *)buffer, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
-        buffer[n] = '\0';
-        printf("Client : %s\n", buffer);
-        sendto(sockfd, (const char *)hello, strlen(hello),  MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
-        printf("Hello message sent.\n"); 
+        n = recvfrom(sockfd, (char *)InMsg, MAXLINE, MSG_WAITALL, (struct sockaddr *) &cliaddr, &len);
+        InMsg[n] = '\0';
+        printf("Client request: %s\n", InMsg);
+        GetFileMetadata(InMsg, OutMsg);
+        sendto(sockfd, (const char *)OutMsg, strlen(OutMsg),  MSG_CONFIRM, (const struct sockaddr *) &cliaddr, len);
+
+        memset(&InMsg, 0, MAXLINE);      //We're preparing buffer for next request
+        memset(&OutMsg, 0, MAXLINE);     //We're preparing buffer for next request
     }
        
     return 0;
